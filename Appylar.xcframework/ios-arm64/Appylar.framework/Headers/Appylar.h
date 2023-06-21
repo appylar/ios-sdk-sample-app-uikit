2e7dc606f7bf4f6989062bc40308fad01012ee13
mayank bhawsar <mayank.bhawsar@cellulant.io>
2023-06-21 16:13:36 +0530

    Added framework for simulator...

--- Appylar.xcframework/ios-arm64/Appylar.framework/Headers/Appylar.h
+++ Appylar.xcframework/ios-arm64/Appylar.framework/Headers/Appylar.h
@@ -8,10 +8,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for Appylar_SDK_iOS.
-FOUNDATION_EXPORT double Appylar_SDK_iOSVersionNumber;
+FOUNDATION_EXPORT double AppylarVersionNumber;
 
 //! Project version string for Appylar_SDK_iOS.
-FOUNDATION_EXPORT const unsigned char Appylar_SDK_iOSVersionString[];
+FOUNDATION_EXPORT const unsigned char AppylarVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Appylar_SDK_iOS/PublicHeader.h>
 
